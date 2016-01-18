f3fbdd0a0bc2a8dbff92d97a1271261dbb39d3cb
Alejandro del Rio <adelriosantiago@gmail.com>
2016-01-17 19:29:45 -0800

    Update git.js

--- git.js
+++ git.js
@@ -1,96 +1,106 @@
 // imports
+var fs = require('fs');
+var path = require('path');
+var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
+var commands = require('./commands');
+var EventEmitter = require('events').EventEmitter;
+var inherits = require('util').inherits;
 
 // Class Git
 var Git = module.exports = function (options) {
   this.binary = 'git';
-  if (typeof options == 'undefined')
-    options = {};
+  options = options || {};
 
-  this.cwd = options.cwd || process.cwd();
-  delete options.cwd;
+  // if a gitDir is provided, make sure we are chdir-ed into
+  // the directory before performing git operations.
+  var gitDir = options['git-dir'];
+  if(gitDir) {
+    var dir = path.dirname(gitDir);
+    var spawn = this.spawn.bind(this);
+    this.spawn = function(command){
+      if(command.indexOf('clone') === 0) {
+        return spawn.apply(this, arguments);
+      }
+
+      process.chdir(dir);
+      return spawn.apply(this, arguments);
+    };
+  }
 
   this.args = Git.optionsToArray(options);
 };
 
-// git.exec(command [[, options], args ], callback)
-Git.prototype.exec = function (command, options, args, callback) {
+inherits(Git, EventEmitter);
+
+Git.prototype.exec = function(command, args, callback) {
   callback = arguments[arguments.length - 1];
-
-  if (arguments.length == 2) {
-    options = {};
+  if(arguments.length == 2) {
     args = [];
-  } else if (arguments.length == 3) {
-    args = arguments[1];
-    options = {};
   }
+  var child = this.spawn(command, args);
+  	
+  child.stdout.setEncoding('utf8');
+  child.stderr.setEncoding('utf8');
 
-  // Put all the args and options together and send as one array to spawn.
-  var cmdArgs = this.args
-      .concat(command)
-      .concat(Git.optionsToArray(options))
-      .concat(Git.optionsToArray(args));
-
-  this.spawnCommand(this.binary, cmdArgs, callback);
-};
-
-/**
- * Spawns command
- *
- * @param {string} binary
- * @param {Array} cmdArgs
- * @param {function} callback
- */
-Git.prototype.spawnCommand = function(binary, cmdArgs, callback) {
-  var gitproc = spawn(binary, cmdArgs, { cwd: this.cwd }),
-      output = '',
-      errorOutput = '';
-
-  // collect stdout
-  gitproc.stdout.on('data', function(data) {
-    output += data;
+  var data = "", 
+	err;
+	
+  child.stdout.on('data', function(d){ 
+    data += d;
+  });
+  child.stderr.on('data', function(e){
+    err += e;  
   });
 
-  // collect stderr
-  gitproc.stderr.on('data', function(data) {
-    errorOutput += data;
-  });
-
-  gitproc.on('close', function(code) {
-    if (code === 0) {
-      callback(null, output);
-    }
-    else {
-      callback(new Error(errorOutput), output);
-    }
-  });
-
-  gitproc.on('error', function(err) {
-    callback(err);
+  child.on('exit', function(){
+    callback(err, data);
   });
 };
 
-// converts an object that contains key value pairs to a args array suitable for child_process.spawn
-Git.optionsToArray = function (options) {
+// git.spawn(command [, args], callback
+Git.prototype.spawn = function(command, args){
+  if(arguments.length == 1) {
+    args = [];
+  }
+  
+  command = Array.isArray(command) ? command : [command];
+  var rawargs = this.args.concat(command).concat(Git.optionsToArray(args));
+  args = [];
+  
+  // Get rid of nulls and undefineds.
+  for(var i = 0, len = rawargs.length; i < len; i++) {
+    if(rawargs[i] != null) args.push(rawargs[i]+"");
+  }
+  
+  var child = spawn(this.binary, args);
+  return child;
+};
+
+Git.optionsToArray = function(options){
   var args = [];
-
-  for (var k in options) {
+  
+  Object.keys(options).forEach(function(k){
     var val = options[k];
 
-    if (k.length == 1) {
-      // val is true, add '-k'
-      if (val === true)
-        args.push('-'+k);
-      // if val is not false, add '-k val'
-      else if (val !== false)
-        args.push('-'+k);
-        args.push(val);
+    if(k.length == 1) {
+      if(val === true) {
+        args.push('-' + k);
+      } else if(val !== false) {
+        args.push('-' + k, val);
+      }
     } else {
-      if (val === true)
-        args.push('--'+k);
-      else if (val !== false)
-        args.push('--'+k+'='+val);
+      if(val === true) {
+        args.push('--' + k);
+      } else if(val !== false) {
+        args.push('--' + k + '=' + val);
+      }
     }
-  }
+  });
+
   return args;
 };
+
+Object.keys(commands).forEach(function(key) {
+  Git.prototype[key] = commands[key];
+});
